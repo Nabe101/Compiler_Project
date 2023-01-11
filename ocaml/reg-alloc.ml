9d69d46d3a9ae9d6ea4a8b89b136608c4a97f59e
ancilloy <yael.ancillon@etu.univ-grenoble-alpes.fr>
2023-01-11 16:44:21 +0100

    (Very) beginning of the register allocation.

---
+++ ocaml/reg-alloc.ml
@@ -0,0 +1,7 @@
+(* Returns the assembly code to load the value in the stack at offset o into register r *)
+let load o r =
+
+(* Returns the assembly code to store the value in register r into the stack at offset o *)
+let store r o =
+
+let rec f code =
