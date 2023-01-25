a6fcc1678342e2031fe8f87057e0a50de764a953
besantej <83356167+besantej@users.noreply.github.com>
2023-01-25 09:27:13 +0100

    Add files via upload

--- java/Main.java
+++ java/Main.java
@@ -5,47 +5,29 @@ import java.util.*;
 public class Main {
   static public void main(String argv[]) {    
     try {
-      /*/ Parser p = new Parser(new Lexer(new FileReader(argv[0])));
+      Parser p = new Parser(new Lexer(new FileReader(argv[0])));
       Exp expression = (Exp) p.parse().value;      
       assert (expression != null);
-
-      Printing AST
+      expression.accept(new AlphaVisitor());
+      expression = expression.accept(new NestedLetVisitor());
+      
       System.out.println("------ AST ------");
       expression.accept(new PrintVisitor());
       System.out.println(); // 
-
-      // Printing AST height
-      System.out.println("------ Height of the AST ----");
-      int height = Height.computeHeight(expression);
-      System.out.println("using Height.computeHeight: " + height);
-
-      ObjVisitor<Integer> v = new HeightVisitor();
-      height = expression.accept(v);
-      System.out.println("using HeightVisitor: " + height); /*/
-
-      Id x = new Id("x"); Id y = new Id("y"); Id z = new Id("z"); Id w = new Id("w"); 
-      /*
-      Exp e = new Let(x, new TInt(), new Int(10),                  // let x:int = 10 in
-              new Let(y, new TInt(), new Int(5),                   // let y:int = 5 in
-              new Let(z, new TInt(), new Add(new Var(x), new Var(y)), // let z:int = x+y
-              new Let(w, new TInt(), new Int(8),                   // let w:int = 8 in
-              new Sub(new Var(z), new Var(w))))));                    // z-w
-      */
-      Exp e = new Let(x, new TInt(), new Int(8),                  // let x:int = 8 in
-              new Let(y, new TInt(), new Int(5),                   // let y:int = 5 in
-              new If((new Not(new Eq(new Var(x), new Var(y)))),       // if (x!=y)
-                new Add(new Var(x), new Var(y)),                        // then x+y
-                new Sub(new Var(x), new Var(y)))));                     // else x-y
-
-      AstToASML converter = new AstToASML();
-      ASML a = converter.convert(e);
+      			
+      ASML a = expression.accept(new AstToASML());
       a.print("out.asml");
 
       // Printing register allocation
-      RegAllocVisitor visit = new RegAllocVisitor();
-      RegAllocation regAll = a.accept(visit);
+      RegAllocVisitor visitReg = new RegAllocVisitor();
+      RegAllocation regAll = a.accept(visitReg);
       regAll.print();
-
+      ArmGen arm = new ArmGen("test", regAll);
+      ArmVisitor visitArm = new ArmVisitor();
+      a.accept(visitArm);
+      System.out.print("\nfin\n");
+      arm.close();
+      
     } catch (Exception e) {
       e.printStackTrace();
     }
